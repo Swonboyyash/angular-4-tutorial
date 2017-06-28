d1cba2dc014fdd889bcfa2ee1f50018b20d08cd3
Swonboyyash <parikhyash366@gmail.com>
2017-06-28 08:56:40 -0700

    minor changes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 import {RecipeService} from '../recipe.service';
@@ -12,21 +12,12 @@ import {RecipeService} from '../recipe.service';
 
 export class RecipeListComponent implements OnInit {
 
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-
   recipes: Recipe[];
-
-  @Output() displayDetails = new EventEmitter<void>();
 
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipe();
-  }
-
-
-  onRecipeSelected(recipeSelected: Recipe) {
-    this.recipeWasSelected.emit(recipeSelected);
   }
 
 
