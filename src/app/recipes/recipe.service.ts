efae416578d4ba53d69afabd0e967eae3f049b28
Yash Parikh <yash.parihk@sandc.com>
2017-06-28 08:49:19 -0700

    minor changes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,24 @@
 import {Recipe} from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
 
   public recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg'),
-    new Recipe('A Test', 'This is simply a test', 'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg'),
+    new Recipe('A Test Recipe',
+     'This is simply a test',
+     'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg',
+      [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+      ]),
+    new Recipe('A Test',
+      'This is simply a test',
+      'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg', [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+    ]),
   ];
 
   getRecipe(): Recipe[] {
